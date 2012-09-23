b20d34a3fc5916578157e924670c66d63194e585
Fabiano Almeida <fabianoalmeida@gmail.com>
2012-09-23 15:23:37 -0300

    Configurated new routes to favorite lists

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Tasks::Application.routes.draw do
   
   match 'public_lists' => 'public_list#index'
 
+  match 'favorite'       => 'favorite_list#create', via: :post
+  match 'unfavorite'     => 'favorite_list#update', via: :put
+  match 'favorite_lists' => 'favorite_list#index'
+
   resources :lists do
     resources :tasks
   end
